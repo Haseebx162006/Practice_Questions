d0f41dce1d2818c5ba667d73228775b15075ccd3
Haseebx162006 <haseebahmad0160@gmail.com>
2025-12-18 00:37:55 +0500

    Questions of Generics added

--- Java Questions/Question1.java
+++ Java Questions/Question1.java
@@ -1,6 +1,17 @@
 public class Question1 {
+    public static <T> void Swap(T[] array, int position1,int position2){
+        position1--;
+        position2--;
+        T temp= array[position1];
+        array[position1]=array[position2];
+        array[position2]=temp;
+    }
     public static void main(String[] args) {
-        System.out.println("My name is Haseeb Ahmad");
+        Integer[] arr= new Integer[]{1,2,3,4};
+        Swap(arr, 1, 3);
+        for(Integer integer: arr){
+            System.out.println(integer);
+        }
     }
     
 }
