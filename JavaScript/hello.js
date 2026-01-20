557a486eb71f3d8f903f3b98e3fd3cb672c78c90
Haseebx162006 <haseebahmad0160@gmail.com>
2026-01-20 11:22:04 +0500

    Js

---
+++ JavaScript/hello.js
@@ -0,0 +1,34 @@
+const numbers = [1, 2, 3, 4, 5, 6];
+let sum=0;
+for(const num of numbers){
+    if(num%2===0){
+        sum+=num
+    }
+}
+console.log(sum)
+
+
+let s="hello"
+let new_string=""
+for(let i =s.length-1; i>=0 ; i--){
+    new_string+=s[i]
+}
+console.log(new_string)
+
+
+
+const arr = [
+  [1, 5, 3],
+  [7, 2, 4],
+  [0, 9, 8]
+];
+
+let largest=0
+for(const num of arr){
+    for(const greatest of num){
+        if(greatest>largest){
+            largest=greatest
+        }
+    }
+}
+console.log(largest)
