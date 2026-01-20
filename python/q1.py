1d907e9c0a331d64570589e8028f4cae9a6c90ce
Haseebx162006 <haseebahmad0160@gmail.com>
2026-01-20 09:01:18 +0500

    Js

---
+++ python/q1.py
@@ -0,0 +1,13 @@
+class Student:
+    def __init__(self,fullname):
+        self.age=fullname
+        print("Hello")
+    def Hello(selfk):
+        print("Hello Haseeb Ahmad Your first day of learning Fastapi")
+
+s1 = Student("Muhammad Muzaffar")
+print(s1.age)
+s1.Hello()
+s2= s1
+s2.age="Youuu"
+print(s1.age,s2.age)
