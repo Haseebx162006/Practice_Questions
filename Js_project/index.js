aae25e60175c7454f5dd47fa1e39b8d6eb8934ea
Haseebx162006 <haseebahmad0160@gmail.com>
2026-01-22 17:27:02 +0500

    Backend day 2

--- Js_project/index.js
+++ Js_project/index.js
@@ -1,10 +1,13 @@
 const express= require('express')
+const path= require('path')
 const app= express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,'public')))
+app.set('view engine',"ejs")
 
 app.get("/",function(req,res,next){
-    res.send("Hello Bhai Server chal raha ha")
+    res.render("index.ejs")
 })
 
 app.listen(3000)
