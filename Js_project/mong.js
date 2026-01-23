bc520eb72c941847136b8aaf895c7b5db31b3a0f
Haseebx162006 <haseebahmad0160@gmail.com>
2026-01-24 00:54:11 +0500

    Backend

--- Js_project/mong.js
+++ Js_project/mong.js
@@ -11,8 +11,8 @@ const userModel=  require('./usermodel')
 app.get("/create", async(req,res)=>{
     let created_user=await userModel.create(
         {
-            name:"Haseeb",
-            username:"haseebx16",
+            name:"Haseeb Muzaffar",
+            username:"haseebx16612",
             email:"haseeahmad",
             password:"Hello_Haseeb"
         }
@@ -20,4 +20,15 @@ app.get("/create", async(req,res)=>{
     res.send(created_user)
 })
 
+app.get("/update",async function(req,res) {
+
+    let updated_user= await userModel.findOneAndUpdate({name:"Haseeb"},{name:"Haseeb Ahmad"},{new:true})
+    res.send(updated_user)
+    
+})
+
+app.get("/read", async(req,res)=>{
+    let users= await userModel.find();
+    res.send(users)
+})
 app.listen(3000)
